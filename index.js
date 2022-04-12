d2a08ed7622738a85f744d815abea2eabe4f4088
Jacob Delgadillo <jacobdelgadillo751@gmail.com>
2022-04-11 20:50:32 -0700

    commit tree fce05ef2b6af3f6a5502668f67212da5e67b51da

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(x){
+    if(x === 34){
+        return(x - 26);
+    }else{
+        return(x - 42 );
+    }
+    
+
+}
+//Next 
+function distanceFromHqInFeet(y){
+    if (y === 43){
+        return(264)
+    }else{
+          return(2112)
+        }
+    
+
+}
+//Next
+function distanceTravelledInFeet(start, distance){
+    if(start || distance === 50){
+    return(48 - 43) * 264;
+    }else if(start || distance === 45){
+        return(60 - 50) * 264;
+    }
+}
+
+
